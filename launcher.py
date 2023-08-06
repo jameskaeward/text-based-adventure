37857b408b148d90b8577df922ac740dfff9463f
James Kaew-ard <james.kaeward@gmail.com>
2023-08-06 15:24:06 +0800

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,14 @@
+from typing import Optional, Tuple, Union
+import customtkinter
+import main
+
+class Launcher(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+    def launch():
+        pass
+
+if __name__ == "__main__":
+    launcher = Launcher()
+    launcher.mainloop()
